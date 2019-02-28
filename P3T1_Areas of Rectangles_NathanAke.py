da1d2914a1e6025afdff9097418d59393b9d7604
aken6319 <47787834+aken6319@users.noreply.github.com>
2019-02-28 10:29:16 -0500

    Add files via upload

---
+++ P3T1_Areas of Rectangles_NathanAke.py
@@ -0,0 +1,25 @@
+#Nathan Ake
+#02/28/2019
+#P3T1
+#This program will take two rectangles and tell you which one has the larger area
+#Need vaibles, input from user, length and width of both rectangles.
+
+#varibles
+Rectangle1 = 0.0
+rectangle2 = 0.0
+
+#Have user input information
+
+Rectangle1 = float(input("Enter the length and width: "))
+Rectangle2 = float(input("Enter the length and width: "))
+
+#calculation
+
+Area1=Rectangle1
+Area2=Rectangle2
+
+#Display the area
+
+print("The area size is", format(Area1,'.2f'),".",sep="")
+print("The area size is", format(Area2,'.2f'),".",sep="")
+
